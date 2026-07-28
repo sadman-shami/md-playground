d407e7342fea26ef7dd290fb1f27ff36bfdd8cce
sadman-shami <srahyme413@gmail.com>
2026-07-28 15:45:13 +0600

    feature: added markdown parser and markdown preview section

---
+++ components/Container.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import LiveEditor from "@/components/LiveEditor";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { markdownToHtml } from "@/lib/markdown";
+
+export default function Container() {
+  const [md, setMD] = useState<string>("");
+  const [html, setHtml] = useState<string>("");
+  const onChange = (md: string | undefined) => {
+    setMD(md ?? "");
+  };
+  useEffect(() => {
+    markdownToHtml(md).then((e) => {
+      setHtml(e);
+      console.log(e);
+    });
+  }, [md]);
+  return (
+    <ResizablePanelGroup className="h-full p-4 gap-2" orientation="horizontal">
+      <ResizablePanel defaultSize={"60%"}>
+        <div className="p-2 h-full col-span-1 bg-foreground/10 backdrop-blur-[5px] rounded-md">
+          <div className="bg-foreground h-full rounded-md p-2">
+            <LiveEditor md={md} onChange={onChange} />
+          </div>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={"40%"}>
+        <div className="p-2 h-full col-span-1 bg-foreground/10 backdrop-blur-[5px] rounded-md">
+          <div className="bg-foreground/15 h-full rounded-md p-2 overflow-auto">
+            {/* biome-ignore lint/security/noDangerouslySetInnerHtml: Trusted HTML from sanitized source. */}
+            <div dangerouslySetInnerHTML={{ __html: html }}></div>
+          </div>
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
+}
---
+++ components/LiveEditor.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { Editor, useMonaco } from "@monaco-editor/react";
+import { useEffect } from "react";
+
+export default function LiveEditor({
+  md,
+  onChange,
+}: {
+  md: string;
+  onChange: (md: string | undefined) => void;
+}) {
+  const monaco = useMonaco();
+  useEffect(() => {
+    if (monaco) {
+      monaco.editor.defineTheme("IDLE", {
+        base: "vs",
+        inherit: false,
+        rules: [
+          {
+            background: "FFFFFF",
+            token: "",
+          },
+          {
+            token: "delimiter",
+            foreground: "999999",
+          },
+          {
+            token: "aaa",
+            foreground: "00ff00",
+          },
+          {
+            foreground: "919191",
+            token: "comment",
+          },
+          {
+            foreground: "00a33f",
+            token: "string",
+          },
+          {
+            foreground: "3b54bf",
+            token: "number",
+          },
+          {
+            foreground: "a535ae",
+            token: "constant.language",
+          },
+          {
+            foreground: "ff5600",
+            token: "keyword",
+          },
+          {
+            foreground: "ff5600",
+            token: "storage",
+          },
+          {
+            foreground: "21439c",
+            token: "entity.name.type",
+          },
+          {
+            foreground: "21439c",
+            token: "entity.name.function",
+          },
+          {
+            foreground: "a535ae",
+            token: "support.function",
+          },
+          {
+            foreground: "a535ae",
+            token: "support.constant",
+          },
+          {
+            foreground: "a535ae",
+            token: "support.type",
+          },
+          {
+            foreground: "a535ae",
+            token: "support.class",
+          },
+          {
+            foreground: "a535ae",
+            token: "support.variable",
+          },
+          {
+            foreground: "000000",
+            background: "990000",
+            token: "invalid",
+          },
+          {
+            foreground: "990000",
+            token: "constant.other.placeholder.py",
+          },
+        ],
+        colors: {
+          "editor.foreground": "#000000",
+          "editor.background": "#ffffff",
+          "editor.selectionBackground": "#BAD6FD",
+          "editor.lineHighlightBackground": "#00000012",
+          "editorCursor.foreground": "#000000",
+          "editorWhitespace.foreground": "#BFBFBF",
+        },
+      });
+      monaco.editor.setTheme("Solarized");
+    }
+  }, [monaco]);
+
+  return (
+    <div className="w-full h-full rounded-md">
+      <Editor
+        theme="Solarized"
+        className="h-full w-full rounded-md"
+        language="markdown"
+        value={md}
+        onChange={(newCode) => onChange(newCode ?? "")}
+        options={{
+          fontFamily: "Geist Mono",
+          fontSize: 18,
+          wordWrap: "on",
+          tabSize: 2,
+          minimap: {
+            enabled: false,
+          },
+          smoothScrolling: true,
+          cursorSmoothCaretAnimation: "on",
+          cursorBlinking: "expand",
+          acceptSuggestionOnEnter: "smart",
+          codeLens: true,
+          contextmenu: false,
+        }}
+      />
+    </div>
+  );
+}
